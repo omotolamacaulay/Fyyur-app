b75d10e38a6fa735f258fa46bd1d8811ef191ab7
Omotola Macaulay <tolamacaulay@gmail.com>
2022-08-09 17:07:47 +0100

    Implement initial migration

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
+from flask_migrate import Migrate
 from forms import *
 #----------------------------------------------------------------------------#
 # App Config.
@@ -21,6 +22,7 @@ moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
 # TODO: connect to a local postgresql database
 
 #----------------------------------------------------------------------------#
